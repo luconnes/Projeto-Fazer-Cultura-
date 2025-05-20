286b2f64742c98ab2251bf9e1921ecec7b36f06b
Vinícius Mergulhão <113155890+Vinimtt@users.noreply.github.com>
2025-05-20 08:36:23 -0300

    Fix: remoção de path desnecessário

--- chat1/chat1/mywebsite/mywebsite/urls.py
+++ chat1/chat1/mywebsite/mywebsite/urls.py
@@ -1,9 +1,7 @@
 # mywebsite/urls.py
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('app_cadastro.urls')),  # home e cadastro de usuários
     path('chat/', include('chat.urls')),     # página do chat
 ]
