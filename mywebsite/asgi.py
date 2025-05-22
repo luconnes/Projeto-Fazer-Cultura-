4a3cb6cceec7599e9aefbc1790d8236519b15b02
Vini <vinimteti@gmail.com>
2025-05-22 16:13:41 -0300

    teste lucas

--- mywebsite/asgi.py
+++ mywebsite/asgi.py
@@ -2,15 +2,17 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing  # Certifique-se de que esse módulo existe!
+import chat.routing  # Certifique-se que o app "chat" está listado no INSTALLED_APPS
 
+# Define o módulo de configurações do Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 
+# Define a aplicação ASGI para lidar com requisições HTTP e WebSocket
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
+    "http": get_asgi_application(),  # Lida com requisições HTTP padrão
+    "websocket": AuthMiddlewareStack(  # Lida com WebSocket, com autenticação
         URLRouter(
-            chat.routing.websocket_urlpatterns  # Certifique-se de que essa URL esteja correta!
+            chat.routing.websocket_urlpatterns  # Roteia as conexões WebSocket
         )
     ),
 })
